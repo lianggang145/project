acccaee9ac37dd793ececff564c0921d174b1f73
xiaozhu <15890956946@163.com>
2017-02-09 09:26:38 +0800

    zhushengnan

---
+++ x09tpl/Application/Home/Controller/ShouController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ShouController extends Controller {
+	public function index(){
+        $this->display('shou/index');
+    }
+}
---
+++ x09tpl/Application/Runtime/Cache/Home/61f53c08a869b3eac3a2cacdf065722c.php
@@ -0,0 +1,77 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link type="text/css" rel="stylesheet" href="/Public/home/css/style.css" />
+    <!--[if IE 6]>
+    <script src="js/iepng.js" type="text/javascript"></script>
+        <script type="text/javascript">
+           EvPNG.fix('div, ul, img, li, input, a'); 
+        </script>
+    <![endif]-->    
+    <script type="text/javascript" src="/Public/home/js/jquery-1.11.1.min_044d0927.js"></script>
+	<script type="text/javascript" src="/Public/home/js/jquery.bxslider_e88acd1b.js"></script>
+    
+    <script type="text/javascript" src="/Public/home/js/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="/Public/home/js/menu.js"></script>    
+        
+	<script type="text/javascript" src="/Public/home/js/select.js"></script>
+    
+	<script type="text/javascript" src="/Public/home/js/lrscroll.js"></script>
+    
+    <script type="text/javascript" src="/Public/home/js/iban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/fban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/f_ban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/mban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/bban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/hban.js"></script>
+    <script type="text/javascript" src="/Public/home/js/tban.js"></script>
+    
+	<script type="text/javascript" src="/Public/home/js/lrscroll_1.js"></script>
+    
+    
+<title>账户激活</title>
+</head>
+<body>  
+<!--Begin Header Begin-->
+<div class="soubg">
+	<div class="sou">
+        <span class="fr">
+        	<span class="fl">你好，请<a href="Login.html">登录</a>&nbsp; <a href="/index.php/Home/user/registr" style="color:#ff4e00;">免费注册</a>&nbsp; </span>
+            <span class="fl">|&nbsp;关注我们：</span>
+            <span class="s_sh"><a href="#" class="sh1">新浪</a><a href="#" class="sh2">微信</a></span>
+            <span class="fr">|&nbsp;<a href="#">手机版&nbsp;<img src="/Public/home/images/s_tel.png" align="absmiddle" /></a></span>
+        </span>
+    </div>
+</div>
+<!--End Header End--> 
+<!--Begin Login Begin-->
+<div class="log_bg">	
+    <div class="top">
+        <div class="logo"><a href="Index.html"><img src="/Public/home/images/logo.png" /></a></div>
+    </div>
+	<div class="login">
+    	<div class="log_img"><img src="/Public/home/images/l_img.png" width="611" height="425" /></div>
+		<div class="log_c">
+
+        <h1><a href="/index.php/Home/user/ji">点我激活账户哦</a></h1>
+        </div>
+    </div>
+</div>
+<!--End Login End--> 
+<!--Begin Footer Begin-->
+<div class="btmbg">
+    <div class="btm">
+        备案/许可证编号：蜀ICP备12009302号-1-www.dingguagua.com   Copyright © 2015-2018 尤洪商城网 All Rights Reserved. 复制必究 , Technical Support: Dgg Group <br />
+        <img src="/Public/home/images/b_1.gif" width="98" height="33" /><img src="/Public/home/images/b_2.gif" width="98" height="33" /><img src="/Public/home/images/b_3.gif" width="98" height="33" /><img src="/Public/home/images/b_4.gif" width="98" height="33" /><img src="/Public/home/images/b_5.gif" width="98" height="33" /><img src="/Public/home/images/b_6.gif" width="98" height="33" />
+    </div>    	
+</div>
+<!--End Footer End -->    
+
+</body>
+
+
+<!--[if IE 6]>
+<script src="//letskillie6.googlecode.com/svn/trunk/2/zh_CN.js"></script>
+<![endif]-->
+</html>
